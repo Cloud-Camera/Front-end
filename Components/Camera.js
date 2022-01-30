6c97b8bd9dea0bead3bf61540afc5bb71bee0959
Chirag2001op <chirag.dogra79@gmail.com>
2022-01-30 14:10:45 +0530

    first

--- Components/Camera.js
+++ Components/Camera.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 const Camera = () => {
-  const cameraPermission = await Camera.getCameraPermissionStatus();
+  
   const newCameraPermission = await Camera.requestCameraPermission()
   const devices = useCameraDevices()
   const device = devices.back
