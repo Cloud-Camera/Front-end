aca54f2a1793ed4f27979cf82f7975551706443d
Chirag2001op <chirag.dogra79@gmail.com>
2022-01-30 11:22:08 +0530

    adding dependencies for react-native-camera

--- Components/Camera.js
+++ Components/Camera.js
@@ -4,13 +4,35 @@ import { Text, View } from 'react-native';
 
 const Camera = () => {
   return (
-    <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-          <RNCamera/>
-      <Text>This is camera</Text>
+    <View style={styles.container}>
+      <RNCamera
+        ref={ref => {
+          this.camera = ref;
+        }}
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        androidRecordAudioPermissionOptions={{
+          title: 'Permission to use audio recording',
+          message: 'We need your permission to use your audio',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        onGoogleVisionBarcodesDetected={({ barcodes }) => {
+          console.log(barcodes);
+        }}
+      />
+      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+        <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+          <Text style={{ fontSize: 14 }}> SNAP </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- android/app/src/main/java/com/cloundcamera/android/MainApplication.java
+++ android/app/src/main/java/com/cloundcamera/android/MainApplication.java
@@ -1,4 +1,4 @@
-package com.nativecamera;
+package com.cloundcamera.android;
 
 import android.app.Application;
 import android.content.Context;
@@ -79,7 +79,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.nativecamera.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.cloundcamera.android.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
