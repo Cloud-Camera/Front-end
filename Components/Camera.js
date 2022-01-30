68ba816cd2c98c4e44f3048a18812382df0ab861
Chirag2001op <chirag.dogra79@gmail.com>
2022-01-30 14:03:37 +0530

    adding vision camera

--- Components/Camera.js
+++ Components/Camera.js
@@ -3,38 +3,19 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 const Camera = () => {
+  const cameraPermission = await Camera.getCameraPermissionStatus();
+  const newCameraPermission = await Camera.requestCameraPermission()
+  const devices = useCameraDevices()
+  const device = devices.back
+
+  if (device == null) return <LoadingView />
   return (
-    <View style={styles.container}>
-      <RNCamera
-        ref={ref => {
-          this.camera = ref;
-        }}
-        style={styles.preview}
-        type={RNCamera.Constants.Type.back}
-        flashMode={RNCamera.Constants.FlashMode.on}
-        androidCameraPermissionOptions={{
-          title: 'Permission to use camera',
-          message: 'We need your permission to use your camera',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-        androidRecordAudioPermissionOptions={{
-          title: 'Permission to use audio recording',
-          message: 'We need your permission to use your audio',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-        onGoogleVisionBarcodesDetected={({ barcodes }) => {
-          console.log(barcodes);
-        }}
-      />
-      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-        <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-          <Text style={{ fontSize: 14 }}> SNAP </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+    />
+  )
 }
 
 export default Camera;
